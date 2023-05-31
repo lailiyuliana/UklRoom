5da2e54b2f4169df8408068c43eaf425d78a4823
SakuraPeach <lailifujiyuliana@gmail.com>
2023-05-31 18:34:32 +0700

    first commit

---
+++ app/src/main/java/com/example/uklroom/database/User.kt
@@ -0,0 +1,16 @@
+package com.example.uklroom.database
+
+import android.os.Bundle
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ColumnInfo(name = "nama") var nama: String,
+    @ColumnInfo(name = "email") var email: String,
+    @ColumnInfo(name = "pass") var pass: String,
+    @ColumnInfo(name = "tanggal") var tanggal: String
+)
